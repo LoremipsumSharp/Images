e9516499ef2a73c357d988697b32183477b3887e
LoremipsumSharp <mvclogic@gmail.com>
2020-07-28 16:32:37 +0800

    Upload by PicGo

---
+++ img/brand.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 192.168.8.34_3316
+ Source Server Type    : MySQL
+ Source Server Version : 50728
+ Source Host           : 192.168.8.34:3316
+ Source Schema         : brand
+
+ Target Server Type    : MySQL
+ Target Server Version : 50728
+ File Encoding         : 65001
+
+ Date: 28/07/2020 16:32:10
+*/
+
